22a28c3965bac0ee0e2a7d39787d86ed51fa968a
Jorge Cano <coronero@gmail.com>
2026-02-09 21:20:09 -0800

    notes 02/09

--- Code/lecture.py
+++ Code/lecture.py
@@ -1,10 +1,21 @@
-def fib(n):
-    if n == 1:
-        return 1
-    if n == 0:
-        return 0
-    return fib(n-1) + fib(n-2)
+import random
 
-print("========")
-r = fib(11)
-print(r)
+trials = 100000        
+students = 50         
+shared_count = 0      
+
+for _ in range(trials):
+    birthdays = []
+
+    # generate birthdays
+    for _ in range(students):
+        day = random.randint(1, 365)
+        birthdays.append(day)
+
+    # check if there is at least one match
+    if len(birthdays) != len(set(birthdays)):
+        shared_count += 1
+
+# estimated probability
+probability = shared_count / trials
+print("Estimated probability:", probability)
