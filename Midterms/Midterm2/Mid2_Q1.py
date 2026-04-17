4ffb07c67c97bac6485bf04efce443de3c353d4a
Jorge Cano <coronero@gmail.com>
2026-04-17 09:41:11 -0700

    Notes 04/17

--- Midterms/Midterm2/Mid2_Q1.py
+++ Midterms/Midterm2/Mid2_Q1.py
@@ -1,12 +1,13 @@
 #1. Write an application that prompts the user to enter a sequence of integer numbers. If the user enters a number that has already been entered before, the application should print all the numbers entered previously and terminate.
 
-q = set()
+a = set()
+num = int(input("enter and integer: "))
 
-u = int(input("Type a number: "))
-
-while u not in q:
-    q.add(u)
-    u = int(input("Type a number: "))
-
-for x in q:
-    print(x)
+while True:
+    if num in a:
+        print(f"You have entered {a} before")
+        break
+    else:
+        a.add(num)
+        num = int(input("enter and integer: "))
+    
--- Midterms/Midterm2/Mid2_Q2.py
+++ Midterms/Midterm2/Mid2_Q2.py
@@ -1,8 +1,9 @@
 #Write a Python application which will construct this plot using matplotlib library.
-from matplotlib import pyplot
+
+import matplotlib.pyplot as pyplot
 
 x = (3, 5, 8, 11, 13, 17, 19)
 y = (50, 35, 45, 25, 35, 20, 25)
 
 pyplot.plot(x, y)
-pyplot.show() 
+pyplot.show()
