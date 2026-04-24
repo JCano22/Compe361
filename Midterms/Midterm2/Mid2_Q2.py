7efad54be6cc8ab7892b9d0457416d7ecd84790b
Jorge Cano <coronero@gmail.com>
2026-04-24 15:59:00 -0700

    notes 04/24

--- Midterms/Midterm2/Mid2_Q2.py
+++ Midterms/Midterm2/Mid2_Q2.py
@@ -5,6 +5,5 @@ import matplotlib.pyplot as pyplot
 x = [3, 5, 8, 11, 13, 17, 19]
 y = [50, 35, 45, 25, 35, 20, 25]
 
-ax = pyplot.axes(projection = '3d')
-ax.plot(x, y)
+pyplot.plot(x, y)
 pyplot.show()
